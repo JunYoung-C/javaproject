7ab138b3a6e40ce05ee4a05a31fd35fa34f5cb56
JunYoung-C <doforme524@gmail.com>
2022-03-07 15:43:28 +0900

    feat: 책 이름으로 조회하는 로직 추가

--- usedbooks/src/usedbooks/purchase/repository/BookRepository.java
+++ usedbooks/src/usedbooks/purchase/repository/BookRepository.java
@@ -2,11 +2,13 @@ package usedbooks.purchase.repository;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import java.util.Vector;
+import javax.crypto.spec.RC2ParameterSpec;
 import usedbooks.purchase.domain.Book;
 
 public class BookRepository {
@@ -38,10 +40,44 @@ public class BookRepository {
     } catch (SQLException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
+    } finally {
+      dbConnect.dbClose(rs, stmt, conn);
     }
 
     return books;
   }
 
   // 이름으로 검색
+  public List<Book> findByName(String BookName) {
+    Connection conn = dbConnect.getOracle();
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    List<Book> books = new Vector<Book>();
+    
+    try {
+      ps = conn.prepareStatement(Sqls.FIND_BOOK_BY_NAME);
+      ps.setString(1, "%" + BookName + "%");
+      rs = ps.executeQuery();
+
+      while (rs.next()) {
+        Long bookId = rs.getLong("book_id");
+        Long MemberId = rs.getLong("member_id");
+        String name = rs.getString("book_name");
+        String author = rs.getString("author");
+        Date publicationDate = rs.getDate("publication_date");
+        int price = rs.getInt("price");
+        String quality = rs.getString("quality");
+
+        books.add(new Book(bookId, MemberId, name, author, publicationDate, price, quality));
+      }
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      dbConnect.dbClose(rs, ps, conn);
+    }
+    
+    return books;
+  }
 }
--- usedbooks/src/usedbooks/purchase/repository/Sqls.java
+++ usedbooks/src/usedbooks/purchase/repository/Sqls.java
@@ -2,5 +2,5 @@ package usedbooks.purchase.repository;
 
 public class Sqls {
   public static final String FIND_ALL_BOOK = "select * from book";
-  
+  public static final String FIND_BOOK_BY_NAME = "select * from book where book_name like ?";
 }
