507c5ad5020be9092b57e091fbdc04cedaa0f075
JunYoung-C <doforme524@gmail.com>
2022-03-08 11:08:02 +0900

    feat: 디자인 통일

--- usedbooks/src/usedbooks/purchase/view/BookInfoView.java
+++ usedbooks/src/usedbooks/purchase/view/BookInfoView.java
@@ -2,6 +2,7 @@ package usedbooks.purchase.view;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -31,13 +32,22 @@ public class BookInfoView extends JFrame implements ActionListener {
 	JTextField searchTextField;
 	JButton searchButton, selectButton, returnButton;
 
+	Font f_title = new Font("맑은 고딕", Font.BOLD, 30);
+	Font f_text = new Font("맑은 고딕", Font.BOLD, 15);
+	Font f_smallText = new Font("맑은 고딕", Font.PLAIN, 11);
+	Color c_title = new Color(49, 130, 246);
+	Color c_white = new Color(255, 255, 255);
+	Color c_black = new Color(27, 29, 31);
+	Color c_gray = new Color(169, 169, 169);
+	Color c_button = new Color(23, 133, 242);
+
 	public BookInfoView(String title) {
 		super(title);
 		cp = this.getContentPane();
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(100, 100, 700, 700);
-		cp.setBackground(new Color(100, 100, 100));
+		this.setBounds(100, 100, 630, 700);
+		cp.setBackground(new Color(255, 255, 255));
 
 		initDesign();
 
@@ -47,10 +57,7 @@ public class BookInfoView extends JFrame implements ActionListener {
 	private void initDesign() {
 		this.setLayout(null);
 
-		titleLabel = new JLabel("중고책 온라인 서점");
-		titleLabel.setBounds(250, 30, 200, 50);
-		this.add(titleLabel);
-
+		setTitleLabel();
 		setSearchArea();
 		setBookTableArea();
 		setSelectAndReturnButton();
@@ -58,15 +65,28 @@ public class BookInfoView extends JFrame implements ActionListener {
 		writeAllBook();
 	}
 
+	private void setTitleLabel() {
+		titleLabel = new JLabel("온라인 중고책 서점", JLabel.CENTER); // 이름 온라인 중고책 서점으로 통일
+		titleLabel.setBounds(110, 50, 400, 40); // 위치,크기 통일
+		titleLabel.setOpaque(false); // 투명
+		titleLabel.setFont(f_title);
+		titleLabel.setForeground(c_title);
+		this.add(titleLabel);
+	}
+
 	private void setSearchArea() {
 		searchLabel = new JLabel("책 이름");
 		searchTextField = new JTextField();
 		searchButton = new JButton("확인");
 
-		searchLabel.setBounds(50, 100, 60, 40);
-		searchTextField.setBounds(160, 100, 300, 40);
-		searchButton.setBounds(500, 100, 60, 40);
+		searchLabel.setBounds(50, 110, 60, 30);
+		searchTextField.setBounds(120, 110, 340, 30);
+		searchButton.setBounds(480, 110, 90, 30);
 
+		searchLabel.setFont(f_text);
+		searchTextField.setFont(f_text);
+		searchButton.setFont(f_text);
+		
 		this.add(searchLabel);
 		this.add(searchTextField);
 		this.add(searchButton);
@@ -78,8 +98,9 @@ public class BookInfoView extends JFrame implements ActionListener {
 		String[] bookFieldnames = { "번호", "책 이름", "지은이", "출판 날짜", "판매가", "상품 상태" };
 		booksTableModel = new DefaultTableModel(bookFieldnames, 0);
 		booksTable = new JTable(booksTableModel);
+		booksTable.setFont(new Font("맑은 고딕", Font.PLAIN, 13));
 		JScrollPane booksScrollpane = new JScrollPane(booksTable);
-		booksScrollpane.setBounds(50, 150, 500, 400);
+		booksScrollpane.setBounds(50, 150, 520, 370);
 		this.add(booksScrollpane);
 	}
 
@@ -87,8 +108,20 @@ public class BookInfoView extends JFrame implements ActionListener {
 		selectButton = new JButton("선택");
 		returnButton = new JButton("돌아가기");
 
-		selectButton.setBounds(150, 600, 100, 40);
-		returnButton.setBounds(300, 600, 100, 40);
+		selectButton.setBounds(140, 570, 150, 50);
+		returnButton.setBounds(340, 570, 150, 50);
+
+		selectButton.setBackground(c_button);
+		selectButton.setForeground(c_white);
+		selectButton.setBorderPainted(false);
+		selectButton.setFocusPainted(false);
+		selectButton.setFont(f_text);
+
+		returnButton.setBackground(c_button);
+		returnButton.setForeground(c_white);
+		returnButton.setBorderPainted(false);
+		returnButton.setFocusPainted(false);
+		returnButton.setFont(f_text);
 
 		this.add(selectButton);
 		this.add(returnButton);
