79cf0723f67b7dcf11e7fa5f15117b9b52be87e0
yongki1403 <dydrlwja55@naver.com>
2022-03-08 11:31:32 +0900

    feet: 글꼴,색상,크기 디자인

--- usedbooks/src/usedbooks/LoginGaip/Login.java
+++ usedbooks/src/usedbooks/LoginGaip/Login.java
@@ -2,6 +2,7 @@ package usedbooks.LoginGaip;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -30,6 +31,24 @@ public class Login extends JFrame implements ActionListener {
 	JLabel labelId, labelPassword, titleLabel;
 
 	JTextField textId, textPassword, textLabel;
+	
+	
+	//Font color
+	Color c_title=new Color(49,130,246);
+	Color c_white=new Color(255,255,255);
+	Color c_black=new Color(27,29,31);
+	Color c_gray=new Color(169,169,169);
+	
+	Color c_button=new Color(23,133,242);
+
+	//Title Font
+	Font f_title = new Font("맑은 고딕",Font.BOLD,30);
+
+	//Text Font
+	Font f_text = new Font("맑은 고딕",Font.BOLD,15);
+	Font f_smallText = new Font("맑은 고딕",Font.PLAIN,11);
+	Font f_bigText = new Font("맑은 고딕", Font.PLAIN, 23);
+
 
 	// memberGaip frame
 	MemberGaip memberframe = new MemberGaip("회원가입");
@@ -39,8 +58,8 @@ public class Login extends JFrame implements ActionListener {
 		cp = this.getContentPane();
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(200, 100, 700, 500);
-		cp.setBackground(new Color(255, 192, 203));
+		this.setBounds(100, 100, 630, 500);
+		cp.setBackground(new Color(255, 255, 255));
 		this.initDesign();
 		this.setVisible(true);
 	}
@@ -48,37 +67,56 @@ public class Login extends JFrame implements ActionListener {
 	public void initDesign() {
 		this.setLayout(null);
 
+	
+		
 		textId = new JTextField();
-		textId.setBounds(220, 120, 300, 30);
+		textId.setBounds(170, 130, 300, 30);
 		textId.setForeground(Color.green);
 		this.add(textId);
 
 		textPassword = new JTextField();
-		textPassword.setBounds(220, 220, 300, 30);
+		textPassword.setBounds(170, 175, 300, 30);
 		textPassword.setForeground(Color.green);
 		this.add(textPassword);
 
-		titleLabel = new JLabel("중고책 온라인 서점", JLabel.CENTER);
-		titleLabel.setBounds(220, 20, 250, 100);
+		titleLabel = new JLabel("온라인 중고책 서점", JLabel.CENTER);
+		titleLabel.setBounds(110, 50, 400, 40);
+		titleLabel.setOpaque(false); // 투명
+		titleLabel.setFont(f_title);
+		titleLabel.setForeground(c_title);
 		this.add(titleLabel);
 
 		labelId = new JLabel("ID", JLabel.LEFT);
-		labelId.setBounds(170, 120, 70, 30);
+		labelId.setBounds(140, 130, 70, 30);
+		labelId.setFont(f_bigText);
+		//labelId.setFont(new Font("맑은 고딕",Font.PLAIN, 12));
 		this.add(labelId);
 
-		labelPassword = new JLabel("PASSWORD", JLabel.LEFT);
-		labelPassword.setBounds(120, 220, 70, 30);
+		labelPassword = new JLabel("PW", JLabel.LEFT);
+		labelPassword.setBounds(130, 175, 70, 30);
+		labelPassword.setFont(f_bigText);
+		//labelPassword.setFont(new Font("맑은 고딕",Font.PLAIN, 12));
 		this.add(labelPassword);
 
 		// 로그인 버튼
 		btnLogin = new JButton("로그인");
-		btnLogin.setBounds(160, 350, 150, 50);
+		btnLogin.setBounds(140, 320, 150, 50);
+		btnLogin.setFont(f_text);
+		btnLogin.setBackground(c_button);
+		btnLogin.setForeground(c_white);
+		btnLogin.setBorderPainted(false);
+		btnLogin.setFocusPainted(false);
 		this.add(btnLogin);
 		btnLogin.addActionListener(this);
 
 		// 회원가입 버튼
 		btnGaip = new JButton("회원가입");
-		btnGaip.setBounds(380, 350, 150, 50);
+		btnGaip.setBounds(340, 320, 150, 50);
+		btnGaip.setFont(f_text);
+		btnGaip.setBackground(c_button);
+		btnGaip.setForeground(c_white);
+		btnGaip.setBorderPainted(false);
+		btnGaip.setFocusPainted(false);
 		this.add(btnGaip);
 		btnGaip.addActionListener(this);
 
--- usedbooks/src/usedbooks/LoginGaip/MemberGaip.java
+++ usedbooks/src/usedbooks/LoginGaip/MemberGaip.java
@@ -2,6 +2,7 @@ package usedbooks.LoginGaip;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -28,14 +29,32 @@ public class MemberGaip extends JFrame implements ActionListener {
 	JTextField tfBirth, tfPhoneNumber, tfAddress;
 
 	JButton btnGaip, btnIdcheck;
+	
+	
+	//Font color
+	Color c_title=new Color(49,130,246);
+	Color c_white=new Color(255,255,255);
+	Color c_black=new Color(27,29,31);
+	Color c_gray=new Color(169,169,169);
+	
+	Color c_button=new Color(23,133,242);
+
+	//Title Font
+	Font f_title = new Font("맑은 고딕",Font.BOLD,30);
+
+	//Text Font
+	Font f_text = new Font("맑은 고딕",Font.BOLD,15);
+	Font f_smallText = new Font("맑은 고딕",Font.PLAIN,11);
+	
+
 
 	public MemberGaip(String title) {
 		super(title);
 		cp = this.getContentPane();
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(200, 100, 320, 370);
-		cp.setBackground(new Color(255, 192, 203));
+		this.setBounds(100, 100, 630, 450);
+		cp.setBackground(new Color(255, 255, 255));
 		this.initDesign();
 		// this.setVisible(true);
 	}
@@ -45,65 +64,73 @@ public class MemberGaip extends JFrame implements ActionListener {
 
 		// label
 		titleLabel = new JLabel("중고책 온라인 서점", JLabel.CENTER);
-		titleLabel.setBounds(10, 10, 270, 20);
+		titleLabel.setBounds(110, 50, 400, 40);
+		titleLabel.setOpaque(false); // 투명
+		titleLabel.setFont(f_title);
+		titleLabel.setForeground(c_title);
 		this.add(titleLabel);
 
 		titleId = new JLabel("아이디");
-		titleId.setBounds(30, 30, 50, 30);
+		titleId.setBounds(190, 100, 250, 30);
 		this.add(titleId);
 
 		titlePassword = new JLabel("비밀번호");
-		titlePassword.setBounds(30, 60, 50, 30);
+		titlePassword.setBounds(190, 132, 90, 30);
 		this.add(titlePassword);
 
 		titleName = new JLabel("이름");
-		titleName.setBounds(30, 90, 50, 30);
+		titleName.setBounds(190, 167, 90, 30);
 		this.add(titleName);
 
 		titleBirth = new JLabel("생일");
-		titleBirth.setBounds(30, 120, 50, 30);
+		titleBirth.setBounds(190, 202, 90, 30);
 		this.add(titleBirth);
 
 		titlePhoneNumber = new JLabel("전화번호");
-		titlePhoneNumber.setBounds(30, 150, 50, 30);
+		titlePhoneNumber.setBounds(190, 237, 90, 30);
 		this.add(titlePhoneNumber);
 
 		titleAddress = new JLabel("주소");
-		titleAddress.setBounds(30, 180, 50, 30);
+		titleAddress.setBounds(190, 272, 90, 30);
 		this.add(titleAddress);
 
 		// tf
 		tfId = new JTextField(4);
-		tfId.setBounds(90, 35, 120, 20);
+		tfId.setBounds(245, 105, 180, 25);
 		this.add(tfId);
 
 		tfPassword = new JTextField(4);
-		tfPassword.setBounds(90, 65, 120, 20);
+		tfPassword.setBounds(245, 138, 180, 25);
 		this.add(tfPassword);
 
 		tfName = new JTextField(4);
-		tfName.setBounds(90, 95, 120, 20);
+		tfName.setBounds(245, 172, 180, 25);
 		this.add(tfName);
 
 		tfBirth = new JTextField(4);
-		tfBirth.setBounds(90, 125, 120, 20);
+		tfBirth.setBounds(245, 206, 180, 25);
 		this.add(tfBirth);
 
 		tfPhoneNumber = new JTextField(4);
-		tfPhoneNumber.setBounds(90, 155, 120, 20);
+		tfPhoneNumber.setBounds(245, 240, 180, 25);
 		this.add(tfPhoneNumber);
 
 		tfAddress = new JTextField(4);
-		tfAddress.setBounds(90, 185, 120, 20);
+		tfAddress.setBounds(245, 280, 180, 25);
 		this.add(tfAddress);
 
 		btnIdcheck = new JButton("중복확인");
-		btnIdcheck.setBounds(220, 35, 85, 20);
+		btnIdcheck.setBounds(430, 105, 100, 25);
 		btnIdcheck.addActionListener(this);
 		this.add(btnIdcheck);
 
 		btnGaip = new JButton("회원가입");
-		btnGaip.setBounds(90, 250, 100, 30);
+		btnGaip.setBounds(240, 330, 150, 50);
+		btnGaip.setFont(f_text);
+		btnGaip.setBackground(c_button);
+		btnGaip.setForeground(c_white);
+		btnGaip.setBorderPainted(false);
+		btnGaip.setFocusPainted(false);
 		this.add(btnGaip);
 	}
 
